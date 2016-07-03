e50e683cd4603c5bd62155ed30e37fc9978e1f8b
ronaldpereira <ronald.drp11@gmail.com>
2016-07-02 21:50:11 -0300

    Correção testbench do banco de registradores

--- TPFinal/register_bank/register_bank.v
+++ TPFinal/register_bank/register_bank.v
@@ -1,6 +1,7 @@
-module register_bank(clock, out, inn, reg_num, enable);
+module register_bank(clock, out, inn, reg_num, enable, enable_a, enable_r);
 
 	input [7:0] enable;
+	input enable_a, enable_r;
 	input clock;
 	input [2:0] reg_num;
 	input [15:0] inn;
@@ -8,7 +9,13 @@ module register_bank(clock, out, inn, reg_num, enable);
 
 	always @ (posedge clock)
 	begin
-		if(enable[reg_num] == 1)
+		if(enable_a == 1)
+			out <= inn;
+
+		else if(enable_r == 1)
+			out <= inn;
+
+		else if(enable[reg_num] == 1)
 			out <= inn;
 	end
 
--- TPFinal/register_bank/testbench.v
+++ TPFinal/register_bank/testbench.v
@@ -3,7 +3,8 @@
 module testbench;
 
 	reg clock = 0;
-	reg [7:0] enable;
+	reg [7:0] enable = 0;
+	reg enable_a = 0, enable_r = 0;
 	reg [2:0] reg_num;
 	reg [15:0] in;
 	wire [15:0] out; // Registrador a ser escrito
@@ -13,19 +14,25 @@ module testbench;
 	initial $dumpfile("testbench.vcd");
 	initial $dumpvars(0, testbench);
 
-	register_bank rb(clock, out, in, reg_num, enable);
+	register_bank rb(clock, out, in, reg_num, enable, enable_a, enable_r);
 
 	initial
 	begin
-		#1 enable = 8'b00000001; // Vetor de enable's passado pelo decoder
+		#0 enable_a = 1;
+		#0 in = 45;
+		#2 enable_a = 0;
+		#0 enable_r = 1;
+		#0 in = 92;
+		#2 enable_r = 0;
+		#3 enable = 8'b00000001; // Vetor de enable's passado pelo decoder
 		#0 in = 256;
 		#0 reg_num = 0;
 		#3 enable = 8'b00010001; // Vetor de enable's passado pelo decoder
 		#0 in = 35;
 		#0 reg_num = 4;
-		#0 in = 22;
+		#2 in = 22;
 		#0 reg_num = 5; // Nao podera escrever no out, pois o bit 5 do enable nao esta ativado
-		#2 $finish;
+		#8 $finish;
 	end
 
 endmodule // testbench
